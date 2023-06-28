f40b380a95a18591d167e91955e553ee4c863e80
VirLg <19virlg@gmail.com>
2023-06-29 01:36:47 +0300

    add

---
+++ src/js/renderPolicy.js
@@ -0,0 +1,14 @@
+// const refBtn = document.querySelector('.policyBTN');
+// const refDiv = document.querySelector('.footer_policy');
+// const a = refBtn.addEventListener('click', onClick);
+
+// function onClick() {
+//   return `
+// <div>spam</div>
+// `.join('');
+// }
+// console.log(a);
+// const renderPolicy = function (params) {
+//   console.log(15);
+// };
+// renderPolicy();
