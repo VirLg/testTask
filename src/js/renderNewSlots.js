a598ac9a20c0b050f3f5191bf42f85c4ae241f17
VirLg <19virlg@gmail.com>
2023-06-29 11:09:14 +0300

    add

--- src/js/renderNewSlots.js
+++ src/js/renderNewSlots.js
@@ -1,43 +1,49 @@
-import imgFive from '../images/5.jpeg';
-import imgSix from '../images/6.jpg';
-import imgSeven from '../images/7.jpeg';
-import imgEight from '../images/8.jpg';
+import six from './six.jpg';
+// import { imgSix } from '/src/images/6.jpg';
+// import { imgSeven } from '../images/7.jpeg';
+// import { imgEight } from '../images/8.jpg';
 
 const refNewSlots = document.querySelector('.nav__item_slots');
 const refDiv = document.querySelector('.footer_policy');
-const a = refNewSlots.addEventListener('click', onClick);
+// const a = refNewSlots.addEventListener('click', onClick);
+// console.log(six);
 
 const gall = [
   {
-    img: imgFive,
+    img: `${six}`,
     title: 'Green Grocery',
-  },
-  {
-    img: imgSix,
-    title: 'Master of Gold',
-  },
-  {
-    img: imgSeven,
-    title: 'Legacy of Doom',
-  },
-  {
-    img: imgEight,
-    title: 'Mummyland Treasures',
   },
 ];
 
-function onClick() {
-  return gall
-    .map(({ img, title }) => {
-      return `
-    
-      <div>
-        <a href=${img}></a>
-        <h3>${title}</h3>
-      </div>
-   
-        `;
-    })
-    .join('');
-  refDiv.insertAdjacentHTML('afterend', onClick);
-}
+// const gall = [imgFive, imgFive, imgFive];
+
+// function onClick(evt) {
+//   evt.preventDefault();
+//   markup();
+// }
+
+// const page = gall
+//   .map(
+//     ({ img, title }) =>
+//       `
+
+//    <div>${img}</div>
+//   <p>${title}<p>
+
+//    `
+//   )
+//   .join('');
+// refDiv.innerHTML = `${six}`;
+
+// const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+// const list = document.querySelector('.list');
+
+// const markup = technologies
+//   .map(technology => `<li class="list-item">${technology}</li>`)
+//   .join('');
+
+// Check the console, you'll see a single string with HTML tags
+// console.log(markup);
+
+// // Adding all the markup in one operation
+// list.innerHTML = markup;
