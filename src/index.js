2702f8d9bc9c6ed7324296b9b78f9ecd80ecb8fc
VirLg <19virlg@gmail.com>
2023-06-29 13:16:30 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import './js/renderPolicy';
-import './js/renderNewSlots';
+// const refNav = document.querySelector('.nav__link_home');
+// console.log(refNav);
+// // body.addE
+// refNav.addEventListener('click', onClick);
+// function onClick(e) {
+//   console.log(e.target);
+//   // e.preventDefault();
+//   if (currentTarget === target) refNav.className.add('activ');
+// }
